214538e89e96e9ff2f24403c0ae399155910ea5a
akthrms <akt.hrms@gmail.com>
2020-12-22 19:47:40 +0900

    update

--- lib/ex_result.ex
+++ lib/ex_result.ex
@@ -99,4 +99,48 @@ defmodule ExResult do
   def map_or_else({:ok, value}, _, fun), do: fun.(value)
 
   def map_or_else({:error, value}, default, _), do: default.(value)
+
+  @spec map_error(result(), (any() -> any())) :: result()
+  @doc """
+  Maps a result to another result by applying a function to a contained error value, leaving an ok value untouched.
+
+  ## Examples
+
+      iex> ExResult.ok(2)
+      ...> |> ExResult.map_error(fn x -> "error code: \#{x}" end)
+      {:ok, 2}
+
+      iex> ExResult.error(13)
+      ...> |> ExResult.map_error(fn x -> "error code: \#{x}" end)
+      {:error, "error code: 13"}
+  """
+  def map_error(result = {:ok, _}, _), do: result
+
+  def map_error({:error, value}, fun), do: error(fun.(value))
+
+  @spec result_and(result(), result()) :: result()
+  @doc """
+  Returns result2 if the result is Ok, otherwise returns the error value of result1.
+
+  ## Examples
+
+      iex> ExResult.result_and(ExResult.ok(2), ExResult.error("late error"))
+      {:error, "late error"}
+
+      iex> ExResult.result_and(ExResult.error("early error"), ExResult.ok("foo"))
+      {:error, "early error"}
+
+      iex> ExResult.result_and(ExResult.error("not a 2"), ExResult.error("late error"))
+      {:error, "not a 2"}
+
+      iex> ExResult.result_and(ExResult.ok(2), ExResult.ok("different result type"))
+      {:ok, "different result type"}
+  """
+  def result_and({:ok, _}, result2 = {:ok, _}), do: result2
+
+  def result_and({:ok, _}, result2 = {:error, _}), do: result2
+
+  def result_and(result1 = {:error, _}, {:ok, _}), do: result1
+
+  def result_and(result1 = {:error, _}, {:error, _}), do: result1
 end
